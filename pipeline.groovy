b098ae07f13bd23cc2d76c5fe208e41438436da4
vvadmin2018 <121259377+vvadmin2018@users.noreply.github.com>
2023-01-21 00:08:31 +0300

    test5

--- pipeline.groovy
+++ pipeline.groovy
@@ -42,13 +42,20 @@ pipeline {
                 git branch: 'main', credentialsId: 'e9f00908-5174-4fa1-82cf-9ca0e3a8c845', url: 'git@github.com:vvadmin2018/demo3.git'
                 //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
 
-                // Run Maven on a Unix agent.
-                //sh "mvn -Dmaven.test.failure.ignore=true clean package"
-
                 // To run Maven on a Windows agent, use
                 bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
 
+        
+        stage("build") {
+      
+            steps {
+                script {
+                  gitCheckout()
+                }
+           }
+
+           }
             post {
                 // If Maven was able to run the tests, even if some of the test
                 // failed, record the test results and archive the jar file.
@@ -59,17 +66,7 @@ pipeline {
             }
         }
 
-    stage("build") {
-      when {expression { return !isFlinsyReleaseBuild()} }
-      steps {
-        script {
 
-          gitCheckout()
-
-        }
-      }
-
-    }
     }   
 }
 def gitCheckout() {
