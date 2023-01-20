b1397ad2d56c9ebe47d7e16aedf32c12c5126c53
vvadmin2018 <121259377+vvadmin2018@users.noreply.github.com>
2023-01-21 01:47:23 +0300

    test8

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,7 +16,7 @@ pipeline {
     stages {
 
 
-      stage("run tests") {
+      stage("docker play") {
         parallel {
             stage('Print Env') {
                 steps {
@@ -28,7 +28,15 @@ pipeline {
                     script {
                       bat "docker info"
                       bat "docker ps"
-                      bat "docker run busybox"  
+                      
+                      echo "--->>> DOCKER IMAGE PULL"
+                      bat "docker rmi busybox:latest"
+                      bat "docker image pull busybox:latest"
+
+                      echo "--->>> DOCKER CONTAINER RUN"
+                      bat "docker run --name mybusybox busybox"  
+                       bat "docker container ls"
+                      
                     }
               }
 
@@ -36,19 +44,30 @@ pipeline {
         }
       }
             
-      stage('Build') {
-            steps {
-                // Get some code from a GitHub repository
-                git branch: 'main', credentialsId: 'e9f00908-5174-4fa1-82cf-9ca0e3a8c845', url: 'git@github.com:vvadmin2018/demo3.git'
-                //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+      stage('Docker try') {
+        steps {
+            script {
+                echo "--->>> DOCKER CONTAINER/IMAGE CLEANUP"
+                bat "docker container stop"
+                bat "docker container rm mybusybox"
+                bat "docker container ls"
+                bat "docker rmi busybox:latest"
+                }
+              }
 
-                // To run Maven on a Windows agent, use
-                bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
-      }
 
-        
-      stage("build") {
+      stage('K8s try') {
+        steps {
+            script {
+                echo "--->>> KUBERNETES VERISON"
+                bat "kubectl version --short"
+                }
+              }
+
+            }
+
+      stage("Build appl") {
             steps {
                 script {
                   gitCheckout()
